cd77516552e8d99ab9340a7342de2b6bc07b55f0
Thomas Dunteman <104242297+LearningToPi@users.noreply.github.com>
2023-11-26 23:29:22 -0700

    fix docker entrypoint for sigterm

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,17 @@
 # If a config file named "config.toml" exists in /app, start using the config file
 # If no config file, pass command line parameters or use defaults
 
-if test -f /app/config.toml; then
-    python3 -m azure_smtp_relay --config /app/config.toml "$@"
+exec() {
+    "$@" &
+    pid="$!"
+    trap 'kill `pidof python3`' SIGTERM
+    wait
+}
+
+if test -f /config.toml; then
+    exec python3 -m azure_smtp_relay --config /config.toml "$@"
+elif test -f /app/config.toml; then
+    exec python3 -m azure_smtp_relay --config /app/config.toml "$@"
 else
-    python3 -m azure_smtp_relay "$@"
+    exec python3 -m azure_smtp_relay "$@"
 fi
