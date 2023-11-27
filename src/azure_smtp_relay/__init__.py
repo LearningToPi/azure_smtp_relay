9a131bd88f0380180c33e2f5e273acc720bfd571
Thomas Dunteman <104242297+LearningToPi@users.noreply.github.com>
2023-11-26 23:34:28 -0700

    v1.0.3 update

--- src/azure_smtp_relay/__init__.py
+++ src/azure_smtp_relay/__init__.py
@@ -94,4 +94,4 @@ from azure.identity import ClientSecretCredential
 from azure.core.credentials import AzureKeyCredential
 from .relay import AzureSmtpRelay
 
-VERSION = (1, 0, 2)    # updated 2023-11-26 23:08:46.577716 from : (1, 0, 1)
+VERSION = (1, 0, 3)    # updated 2023-11-26 23:32:48.049371 from : (1, 0, 2)
